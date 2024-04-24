f429bf9045f63614f544f862bf7fcb2c106bbd1c
JordanGallant <39-12602@sae.edu.za>
2024-04-24 09:48:05 +0200

    change-back

--- scroll/hardhat.config.ts
+++ scroll/hardhat.config.ts
@@ -1,8 +1,28 @@
+import * as dotenv from "dotenv";
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config();
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      evmVersion: "london"
+    }
+  },
+  networks: {
+    scrollTestnet: {
+      url: "https://sepolia-rpc.scroll.io/",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
 
 export default config;
