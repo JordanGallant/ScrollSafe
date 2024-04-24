5c5e54db1357e18b52055810a2e03d262535c511
JordanGallant <39-12602@sae.edu.za>
2024-04-24 09:35:32 +0200

    fix-error

--- scroll/hardhat.config.ts
+++ scroll/hardhat.config.ts
@@ -1,28 +1,8 @@
-import * as dotenv from "dotenv";
-
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-dotenv.config();
-
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.9",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-      evmVersion: "london"
-    }
-  },
-  networks: {
-    scrollTestnet: {
-      url: process.env.SCROLL_TESTNET_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
+  solidity: "0.8.24",
 };
 
 export default config;
